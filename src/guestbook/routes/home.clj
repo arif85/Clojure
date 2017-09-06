587fa4563e9597a1f28a62b4821c05836880c655
Huseinov <huseinov.arif@gmail.com>
2017-09-06 16:12:06 +0300

    db created

--- src/guestbook/routes/home.clj
+++ src/guestbook/routes/home.clj
@@ -1,10 +1,50 @@
 (ns guestbook.routes.home
   (:require [compojure.core :refer :all]
-            [guestbook.views.layout :as layout]))
+            [guestbook.views.layout :as layout]
+            [hiccup.form :refer :all]
+            [guestbook.models.db :as db]))
 
-(defn home []
-  (layout/common [:h1 "Hello World of Clojure!"]
-                  [:h1 (str"this is 'p' tag! " (+ 1 2)) ]))
+(defn show-guests []
+  [:ul.guests
+   (for [{:keys [message name timestamp]}
+         [{:message "Howdy" :name "Bob" :timestamp nil}
+          {:message "Hello" :name "Bob" :timestamp nil}]]
+     [:li
+      [:blockquote message]
+      [:p "-" [:cite name]]
+      [:time timestamp]])])
+
+
+(defn home [& [name message error]]
+  (layout/common [:h1 "GuestBook"]
+                 [:p "Welcome to my guestbook"]
+                 [:p error]
+                 (show-guests)
+                 [:hr]
+                 (form-to [:post "/"]
+                          [:p "Name"]
+                          (text-field "name" name)
+                          ;;[:input]
+                          [:p "Message"]
+                          (:text-area  "message" message)
+                          [:textarea {:rows 10 :cols 40} "message" message]
+                          [:br]
+                          (submit-button "comment"))))
+
+
+(defn save-message [name message]
+  (cond
+    (empty? name)
+    (home name message "Some dummy forgot to leave a name")
+    (empty? message)
+    (home name message "Don't you have something to say?")
+    :else
+    (do
+      (println name message )
+      (home))))
+
 
 (defroutes home-routes
-  (GET "/" [] (home)))
+           (GET "/" [] (home))
+           (POST "/" [name message] (save-message name message)))
+
